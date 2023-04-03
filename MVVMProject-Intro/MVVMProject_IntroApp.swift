3aab8664d6a7bc6820800f80f7b0b60ed3872493
blakeCS <mburton@blakeschool.org>
2023-04-03 10:10:36 -0500

    Initial Commit

---
+++ MVVMProject-Intro/MVVMProject_IntroApp.swift
@@ -0,0 +1,17 @@
+//
+//  MVVMProject_IntroApp.swift
+//  MVVMProject-Intro
+//
+//  Created by Madeline Burton on 4/3/23.
+//
+
+import SwiftUI
+
+@main
+struct MVVMProject_IntroApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
